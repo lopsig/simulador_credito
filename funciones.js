3895536fd7788408ba40796825e7a23f59ce3f22
lopsig <jonathan.ls.trabajo@gmail.com>
2026-04-18 16:42:29 -0500

    creating functions to calculate the loan

--- funciones.js
+++ funciones.js
@@ -1,1 +1,76 @@
-//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+// CALCULATE FUNCTIONS
+const calculateAvailableBalance = (income, expenses) => {
+  let availableBalance = parseFloat(income - expenses).toFixed(2);
+
+  if (availableBalance < 0) {
+    return "0.00";
+  }
+
+  return availableBalance;
+};
+
+const calculateAbilityPay = (availableBalance) => {
+  let abilityPay = parseFloat(availableBalance * 0.5).toFixed(2);
+
+  if (abilityPay <= 0) {
+    return "0.00";
+  }
+  return abilityPay;
+};
+
+const calculateSimpleInterest = (amount, rate, term) => {
+  interestRate = rate / 100;
+
+  let simpleInterest = term * amount * interestRate;
+
+  return simpleInterest;
+};
+
+const calculateTotal = (amount, simpleInterest) => {
+  let total = amount + simpleInterest + 100
+
+  return total
+}
+
+const calculateMonthlyPayment = (total, term) => {
+  let monthlyTerm = term * 12;
+  let monthlyPayment = parseFloat(total / monthlyTerm).toFixed(2)
+
+  return monthlyPayment
+}
+
+const approveCredit = (abilityPay, monthlyPayment) => {
+  if (abilityPay > monthlyPayment) {
+    return true
+  } else {
+    return false
+  }
+
+}
+
+//UTIL FUNCTIONS
+const recoverText = (idComponent) => {
+  let component = document.getElementById(idComponent);
+  let value = component.value;
+
+  return value;
+};
+
+const recoverFloat = (idComponent) => {
+  let valueText = recoverText(idComponent);
+  let valueFloat = parseFloat(valueText);
+
+  return valueFloat;
+};
+
+const recoverInt = (idComponent) => {
+  let valueText = recoverText(idComponent);
+  let valueInt = parseInt(valueText);
+
+  return valueInt;
+};
+
+const showSpan = (idComponent, value) => {
+  let component = document.getElementById(idComponent);
+  component.textContent = "USD " + value;
+};
--- simulador.js
+++ simulador.js
@@ -1,1 +1,26 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+const calculate = () => {
+  let income = recoverFloat("txtIngresos");
+  let expenses = recoverFloat("txtEgresos");
+
+  let balance = calculateAvailableBalance(income, expenses);
+
+  showSpan("spnDisponible", balance);
+
+  let abilityPay = calculateAbilityPay(balance);
+
+  showSpan("spnCapacidadPago", abilityPay);
+
+  let amount = recoverInt("txtMonto");
+  let rate = recoverInt("txtTasaInteres");
+  let term = recoverInt("txtPlazo");
+
+  let simpleInterest = calculateSimpleInterest(amount, rate, term)
+  showSpan("spnInteresPagar", simpleInterest);
+
+  let total = calculateTotal(amount, simpleInterest)
+  showSpan("spnTotalPrestamo", total);
+
+  let monthlyPayment = calculateMonthlyPayment(total, term)
+  showSpan("spnCuotaMensual", monthlyPayment);
+
+};
